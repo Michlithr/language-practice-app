cfc15aded0931b39f6c58ea3c4f8267009e5be59
Michal Litwa <michal.litwa@qiagen.com>
2024-05-15 12:26:02 +0200

    #3 created header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,9 @@
-import Head from "next/head";
+import Header from "@ui/header";
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen min-w-screen">
-      <Head>
-        <title>Home Page</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <header className="flex justify-center items-center min-w-full h-24 p-0 bg-lapis-lazuli text-white font-bold">
-        <p>Header</p>
-      </header>
+      <Header />
       <main className="min-w-full flex-grow bg-white">
         <p>Content</p>
       </main>
